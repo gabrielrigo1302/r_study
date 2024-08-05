10eece7dce579802714ace30805db4b8a6220269
Gabriel <gabrieloliveirarigo13@gmail.com>
2024-08-05 19:46:11 -0300

    Chapter 1: concluded

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\gabri\OneDrive - Associacao Antonio Vieira\Documentos\Projetos\r_study\hello_cargo\target\debug\deps\hello_cargo.exe: src/main.rs
+
+C:\Users\gabri\OneDrive - Associacao Antonio Vieira\Documentos\Projetos\r_study\hello_cargo\target\debug\deps\hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\gabri\OneDrive\ -\ Associacao\ Antonio\ Vieira\Documentos\Projetos\r_study\hello_cargo\target\debug\hello_cargo.exe: C:\Users\gabri\OneDrive\ -\ Associacao\ Antonio\ Vieira\Documentos\Projetos\r_study\hello_cargo\src\main.rs
